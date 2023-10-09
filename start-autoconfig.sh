7bbb7bd82b9990330c95aedd83d6b4212a71fbe8
Raúl Martín <raul.martin@urjc.es>
2023-10-09 19:55:35 +0200

    Autoconfig: updated scenario, runner.R and util script start-autoconfig.sh

---
+++ start-autoconfig.sh
@@ -0,0 +1,10 @@
+set -e
+mvn clean package
+java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.rmi.port=9010 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false \
+-Djava.rmi.server.hostname=192.168.10.114 \
+-Xmx24G \
+-Xms24G \
+-jar target/BMSSC-0.19-SNAPSHOT.jar \
+--autoconfig \
+--solver.parallelExecutor=true \
+--solver.nWorkers=16

e33ebec1c20cb3029a58f632199f62ad7e99ff64
Raúl Martín <raul.martin@urjc.es>
2025-01-18 03:40:02 +0100

    partial compl tests

--- start-complexity.sh
+++ start-complexity.sh
@@ -1,12 +1,14 @@
+# Requires libcurl4-openssl-dev in most Ubuntu distributions, install before launching
 set -e
 mvn clean package
 java \
 -Xmx24G \
 -Xms24G \
 -jar target/BMSSC-0.21-SNAPSHOT.jar \
+--instances.path.default=instances/tuning/instances \
 --serializers.solution-json.enabled=true \
 --serializers.solution-json.frequency=all \
 --solver.experiments=FinalExperiment \
 --solver.parallelExecutor=true \
---solver.nWorkers=8 \
+--solver.nWorkers=32 \
 --solver.metrics=true
